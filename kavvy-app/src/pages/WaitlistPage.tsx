a5765de60ec2617fc207ec987a89e40906a78249
Shashwat Raj <rajayshashwat@gmail.com>
2026-02-27 11:13:18 -0700

    minor waitlist page changes

--- kavvy-app/src/pages/WaitlistPage.tsx
+++ kavvy-app/src/pages/WaitlistPage.tsx
@@ -147,7 +147,7 @@ export function WaitlistPage() {
               <path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"/>
             </svg>
             <h4>Smart Matching</h4>
-            <p>AI-powered recommendations to find publishers that fit your work</p>
+            <p>AI recommendations to find publishers that fit and appreciate your work</p>
           </div>
 
           <div className="waitlist-feature">
